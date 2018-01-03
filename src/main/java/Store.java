1089e3def1bd5382f68d45ec7533da4c9c2bff3b
ava <avabe@post.bgu.ac.il>
2018-01-03 20:04:51 +0200

    20:07

---
+++ src/main/java/Store.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.List;
+
+public class Store {
+
+    private HashMap<String, Movie> _movies;
+    private HashMap<String, User> _clients;
+
+    public Store(HashMap<String, Movie> movies, HashMap<String, User> clients){
+        _movies= movies;
+        _clients= clients;
+    }
+}
