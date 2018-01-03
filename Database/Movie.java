5c619f71a9c4f50936df29eceef7b0ae2da7c08e
kadoshy <kadosh01@gmail.com>
2018-01-03 09:45:20 -0800

    10:22

--- Database/Movie.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public class Movie {
-
-    public Movie(String name, int price , String id, List<String> BannedCategories ,int availableAmount,int totalAm){
-
-    }
-}
---
+++ src/main/java/Movie.java
@@ -0,0 +1,65 @@
+import java.util.List;
+
+public class Movie {
+
+    private String _name,_id;
+    private int _price,_availableAmount,_totalAmount;
+    private List<String> _bannedCategories;
+
+    public Movie(String name, int price , String id, List<String> bannedCategories ,int availableAmount,int totalAmount){
+        _availableAmount=availableAmount;
+        _bannedCategories=bannedCategories;
+        _id=id;
+        _name=name;
+        _price=price;
+        _totalAmount=totalAmount;
+    }
+
+    public String get_name() {
+        return _name;
+    }
+
+    public void set_name(String _name) {
+        this._name = _name;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public int get_price() {
+        return _price;
+    }
+
+    public void set_price(int _price) {
+        this._price = _price;
+    }
+
+    public int get_availableAmount() {
+        return _availableAmount;
+    }
+
+    public void set_availableAmount(int _availableAmount) {
+        this._availableAmount = _availableAmount;
+    }
+
+    public int get_totalAmount() {
+        return _totalAmount;
+    }
+
+    public void set_totalAmount(int _totalAmount) {
+        this._totalAmount = _totalAmount;
+    }
+
+    public List<String> get_bannedCategories() {
+        return _bannedCategories;
+    }
+
+    public void set_bannedCategories(List<String> _bannedCategories) {
+        this._bannedCategories = _bannedCategories;
+    }
+}
