64818e8628b08bc4fe260213dc169ffe6bf46d14
kadoshy <kadosh01@gmail.com>
2018-01-03 09:13:44 -0800

    10:22

--- src/main/java/bgu/spl181/net/srv/UserServiceTextBasedProtocol.java
+++ src/main/java/bgu/spl181/net/srv/UserServiceTextBasedProtocol.java
@@ -3,7 +3,7 @@ package bgu.spl181.net.srv;
 import bgu.spl181.net.api.bidi.BidiMessagingProtocol;
 import bgu.spl181.net.api.bidi.Connections;
 
-public class BidiMessagingProtocolImpl<String> implements BidiMessagingProtocol<String> {
+public class UserServiceTextBasedProtocol<String> implements BidiMessagingProtocol<String> {
     @Override
     public void start(int connectionId, Connections<String> connections) {
 
