1dd41401a7787146089cae8b07418493d013e54c
ava <avabe@post.bgu.ac.il>
2018-01-03 19:34:15 +0200

    19:36

---
+++ Database/User.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class User {
+
+    private String _username;
+    private String _password;
+    private String _type;
+    private String _country;
+    private List<Movie> _rentedMovies;
+    private int _balance;
+
+    public User(String username, String type, String password, String country, List<Movie> rentedMovies, int balance){
+        _username= username;
+        _type=type;
+        _password= password;
+        _country= country;
+        _rentedMovies= rentedMovies;
+        _balance= balance;
+
+    }
+
+}
