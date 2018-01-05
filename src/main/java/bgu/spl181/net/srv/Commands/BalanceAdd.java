071dddda88d922465cca26175f3cdd6fff4877a2
ava <avabe@post.bgu.ac.il>
2018-01-05 18:47:32 +0200

    18:47

--- src/main/java/bgu/spl181/net/srv/Commands/BalanceAdd.java
+++ src/main/java/bgu/spl181/net/srv/Commands/BalanceAdd.java
@@ -5,9 +5,13 @@ import bgu.spl181.net.srv.Database;
 
 public class BalanceAdd extends Request{
 
+    private int _connectionId;
+    private int _amount;
 
     public BalanceAdd(Connections connections, Database database, int connectionId, int amount) {
         super(connections, database);
+        _connectionId= connectionId;
+        _amount= amount;
     }
 
     @Override
